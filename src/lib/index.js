08aadf5c9016073fc758ff752b32c24da6a432ce
CYNTHIAMOR <kabita262011@hotmail.com>
2023-06-20 13:29:10 -0500

    traer cambios

--- src/lib/index.js
+++ src/lib/index.js
@@ -20,7 +20,7 @@ export function crearUsuarioConCorreoYContraseña(email, contraseña, onNavigate
         onNavigate('/post');
       }
     });
-
+  }
 // INICIAR SESION CON CORREO Y CONTRASE;A
 
 export function signIn(email, contraseña, onNavigate) {
@@ -54,74 +54,3 @@ export const createPost = (contenido) => {
     usuario: auth.currentUser.email,
   });
 };
-
-// OBTENER LOS POST
-
-export querySnapshot = getDocs(collection(db, "users"));
-querySnapshot.then((docs)=>{
-  console.log(docs)
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* export const obtenTodosLosPost = (maa) =>{
-  db.collection("users").get().then((querySnapshot) => {
-    const post = [];
-    querySnapshot.forEach((doc) => {
-        [post.push](`${doc.id} => ${doc.data()}`);
-    });
-    maa();
-});
-} */
-
-// Posteo
-/* export const readPostHome = (callback) => {
-  firebase.firestore().collection('posts').where('privacy', '==', '1')
-   // .orderBy('orderDate', 'desc')
-    .onSnapshot((querySanpshot) => {
-      const post = [];
-      querySanpshot.forEach((doc) => {
-        post.push({ id: doc.id, ...doc.data() });
-      });
-      callback(post);
-    });
-};
-
-// READ POSTS PROFILE
-export const readPostProfile = (uid, callback) => {
-  firebase.firestore().collection('posts').where('uid', '==', uid)
-    .orderBy('orderDate', 'desc')
-    .onSnapshot((querySanpshot) => {
-      const post = [];
-      querySanpshot.forEach((doc) => {
-        post.push({ id: doc.id, ...doc.data() });
-      });
-      callback(post);
-    });
-}; */
