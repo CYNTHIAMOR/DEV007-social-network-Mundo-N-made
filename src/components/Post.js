ab1a33ad49d61ad4d1ba8f93ddd85ac06c68694c
CYNTHIAMOR <kabita262011@hotmail.com>
2023-06-21 20:42:53 -0500

    cambioCssNoTerminado

--- src/components/Post.js
+++ src/components/Post.js
@@ -24,7 +24,14 @@ export const Post = (onNavigate) => {
   HomeDiv.innerHTML = `
 <div class="container-background">
 <div class="container-presentation">
-  <div class="container-logo"></div>
+<div class="container-logo"></div>
+<button class="btn-finish"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-logout-2" width="30" height="44" viewBox="0 0 24 24" stroke-width="2.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+<path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+<path d="M10 8v-2a2 2 0 0 1 2 -2h7a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-7a2 2 0 0 1 -2 -2v-2" />
+<path d="M15 12h-12l3 -3" />
+<path d="M6 15l-3 -3" />
+</svg></button>
+  
 
 </div>
  
